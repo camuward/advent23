5bc5d64fe3bb37837d22478e0a2f46d544bc352e
Cameron Ward <cameronuward@gmail.com>
2023-12-02 22:21:28 -0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+use yaah::{aoc, aoc_lib, aoc_year};
+
+aoc_year!(2023);
+
+#[aoc(day1, part1)]
+fn day1_part1(_input: &'static str) -> usize {
+    todo!()
+}
+
+aoc_lib!(with_benchmarks);
---
+++ src/main.rs
@@ -0,0 +1,1 @@
+yaah::aoc_main! { advent }
