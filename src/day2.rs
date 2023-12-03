1bb7d80d083bd26caf21a3d21b5273118d299af4
Cameron Ward <cameronuward@gmail.com>
2023-12-02 23:40:32 -0500

    day2p2: solve and clean up a lil

--- src/day2.rs
+++ src/day2.rs
@@ -5,30 +5,51 @@ fn parse_id(id: &str) -> Option<u32> {
 }
 
 fn count_per_color(subset: &str) -> [u32; 3] {
-    subset.split(", ").fold([0, 0, 0], |[mut r, mut g, mut b], count_color| {
-        let (count, color) = count_color.split_once(' ').expect("no count");
-        let count: u32 = count.parse().expect("count is not a number");
+    subset
+        .split(", ")
+        .fold([0, 0, 0], |[mut r, mut g, mut b], count_color| {
+            let (count, color) = count_color.split_once(' ').expect("no color count");
+            let count: u32 = count.parse().expect("count is not a number");
 
-        match color {
-            "red" => r += count,
-            "green" => g += count,
-            "blue" => b += count,
-            _ => unreachable!("unknown color"),
-        };
+            match color {
+                "red" => r += count,
+                "green" => g += count,
+                "blue" => b += count,
+                _ => unreachable!("unknown color"),
+            };
 
-        [r, g, b]
-    })
+            [r, g, b]
+        })
 }
 
 #[yaah::aoc(day2, part1)]
 fn part_one(input: &str) -> u32 {
-    let possible = input.lines().zip(1..).filter_map(|(line, id)| {
+    let possible_games = input.lines().zip(1..).filter_map(|(line, id)| {
         let (_id, set_of_cubes) = line.split_once(": ").expect("no game id");
         debug_assert_eq!(parse_id(_id), Some(id));
 
         let mut subsets = set_of_cubes.split("; ").map(count_per_color);
-        subsets.all(|[r, g, b]| r <= 12 && g <= 13 && b <= 14).then(|| id)
+        subsets
+            .all(|[r, g, b]| r <= 12 && g <= 13 && b <= 14)
+            .then(|| id)
     });
 
-    possible.sum()
+    possible_games.sum()
+}
+
+#[yaah::aoc(day2, part2)]
+fn part_two(input: &str) -> u32 {
+    let powers = input.lines().map(|line| {
+        let (_id, set_of_cubes) = line.split_once(": ").expect("no game id");
+
+        let subsets = set_of_cubes.split("; ").map(count_per_color);
+        let required = subsets
+            .reduce(|[r1, g1, b1], [r2, g2, b2]| [r1.max(r2), g1.max(g2), b1.max(b2)])
+            .expect("game record empty");
+
+        let [r, g, b] = required;
+        r * g * b
+    });
+
+    powers.sum()
 }
