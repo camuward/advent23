db0f43eb441a1a783a1e09b262e7d8d6e1db6f3f
Cameron Ward <cameronuward@gmail.com>
2023-12-04 04:32:34 -0500

    day4p2: pretty decent solution

--- src/day4.rs
+++ src/day4.rs
@@ -25,3 +25,33 @@ fn part_one(input: &str) -> u32 {
         })
         .sum()
 }
+
+#[yaah::aoc(day4, part2, naive)]
+fn part_two(input: &str) -> u32 {
+    let count = input.lines().count();
+    let mut copies = vec![1; count];
+
+    for (current_line, line) in input.lines().enumerate() {
+        let (_card, contents) = line.split_once(':').expect("no card id");
+        let (winning, your_hand) = contents.split_once('|').expect("no winning separator");
+
+        let winning: Vec<u32> = winning
+            .split_whitespace()
+            .map(|s| s.parse())
+            .collect::<Result<_, _>>()
+            .expect("invalid winning number");
+
+        let count_winning = your_hand
+            .split_whitespace()
+            .map(|s| s.parse().expect("invalid card number"))
+            .filter(|card| winning.contains(card))
+            .count();
+
+        let this_card_copies = copies[current_line];
+        for card_copies in copies.iter_mut().skip(current_line + 1).take(count_winning) {
+            *card_copies += this_card_copies;
+        }
+    }
+
+    copies.into_iter().sum()
+}
