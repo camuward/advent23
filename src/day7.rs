b0b86fa336badc7d69d6640dd902cb66c3838e25
Cameron Ward <cameronuward@gmail.com>
2023-12-10 18:42:16 -0500

    day7p2: big awful match

--- src/day7.rs
+++ src/day7.rs
@@ -72,3 +72,54 @@ fn part_one(input: &str) -> u32 {
         .map(|(rank, hand)| rank * hand.bid() as u32)
         .sum()
 }
+
+#[yaah::aoc(day7, part2)]
+fn part_two(input: &str) -> u32 {
+    let mut hands: Vec<Hand> = input
+        .lines()
+        .map(|line| (&line.as_bytes()[0..5], &line[6..]))
+        .map(|(hand, bid)| {
+            let cards = {
+                let mut buf = [0; 5];
+                let mut cards = hand.into_iter().map(|card| match card {
+                    b'A' => 12,
+                    b'K' => 11,
+                    b'Q' => 10,
+                    b'J' => 0,
+                    b'T' => 9,
+                    b'2'..=b'9' => card - b'2' + 1,
+                    _ => unreachable!(),
+                });
+                buf.fill_with(|| cards.next().unwrap());
+
+                buf
+            };
+
+            let counts = cards.into_iter().fold([0; 13], |mut counts, card| {
+                counts[card as usize] += 1;
+                counts
+            });
+
+            let max = counts.into_iter().skip(1).max().unwrap();
+            let kind = match (counts[0], max) {
+                (5, _) | (4, _) | (3, 2) | (2, 3) | (1, 4) | (_, 5) => Hand::FiveOfKind,
+                (3, 1) | (2, 2) | (1, 3) | (0, 4) => Hand::FourOfKind,
+                (1, _) if counts.iter().filter(|&&c| c == 2).count() == 2 => Hand::FullHouse,
+                (_, 3) if counts.contains(&2) => Hand::FullHouse,
+                (2, 1) | (1, 2) | (0, 3) => Hand::ThreeOfKind,
+                (0, 2) if counts.iter().filter(|&&c| c == 2).count() == 2 => Hand::TwoPair,
+                (1, 1) | (0, 2) => Hand::Pair,
+                (0, 1) => Hand::HighCard,
+                _ => unreachable!(),
+            };
+
+            kind(cards, bid.parse().expect("invalid bid"))
+        })
+        .collect();
+
+    hands.sort_unstable();
+    (1..)
+        .zip(hands)
+        .map(|(rank, hand)| rank * hand.bid() as u32)
+        .sum()
+}
