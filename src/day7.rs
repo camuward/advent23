f7e5a588627e7a6086fd038948dcb65d736ade8c
Cameron Ward <cameronuward@gmail.com>
2023-12-10 18:40:06 -0500

    day7p1: move parse logic to fn

--- src/day7.rs
+++ src/day7.rs
@@ -24,58 +24,49 @@ impl Hand {
     }
 }
 
-fn parse_card(card: &u8) -> u8 {
-    match card {
-        b'A' => 12,
-        b'K' => 11,
-        b'Q' => 10,
-        b'J' => 9,
-        b'T' => 8,
-        b'2'..=b'9' => card - b'2',
-        _ => unreachable!(),
-    }
-}
-
-fn parse_hand(hand: &[u8], bid: u16) -> Hand {
-    assert_eq!(hand.len(), 5);
-
-    let cards = {
-        let mut buf = [0; 5];
-        let mut cards = hand.into_iter().map(parse_card);
-        buf.fill_with(|| cards.next().unwrap());
-
-        buf
-    };
-
-    let counts = cards.into_iter().fold([0; 13], |mut counts, card| {
-        counts[card as usize] += 1;
-        counts
-    });
-
-    let kind = match counts.into_iter().max().unwrap() {
-        5 => Hand::FiveOfKind,
-        4 => Hand::FourOfKind,
-        3 if counts.contains(&2) => Hand::FullHouse,
-        3 => Hand::ThreeOfKind,
-        _ if counts.iter().filter(|&&c| c == 2).count() == 2 => Hand::TwoPair,
-        2 => Hand::Pair,
-        1 => Hand::HighCard,
-        _ => unreachable!(),
-    };
-
-    kind(cards, bid)
-}
-
 #[yaah::aoc(day7, part1)]
 fn part_one(input: &str) -> u32 {
     let mut hands: Vec<Hand> = input
         .lines()
         .map(|line| (&line.as_bytes()[0..5], &line[6..]))
-        .map(|(hand, bid)| parse_hand(hand, bid.parse::<u16>().expect("invalid bid")))
+        .map(|(hand, bid)| {
+            let cards = {
+                let mut buf = [0; 5];
+                let mut cards = hand.into_iter().map(|card| match card {
+                    b'A' => 12,
+                    b'K' => 11,
+                    b'Q' => 10,
+                    b'J' => 9,
+                    b'T' => 8,
+                    b'2'..=b'9' => card - b'2',
+                    _ => unreachable!(),
+                });
+                buf.fill_with(|| cards.next().unwrap());
+
+                buf
+            };
+
+            let counts = cards.into_iter().fold([0; 13], |mut counts, card| {
+                counts[card as usize] += 1;
+                counts
+            });
+
+            let kind = match counts.into_iter().max().unwrap() {
+                5 => Hand::FiveOfKind,
+                4 => Hand::FourOfKind,
+                3 if counts.contains(&2) => Hand::FullHouse,
+                3 => Hand::ThreeOfKind,
+                _ if counts.iter().filter(|&&c| c == 2).count() == 2 => Hand::TwoPair,
+                2 => Hand::Pair,
+                1 => Hand::HighCard,
+                _ => unreachable!(),
+            };
+
+            kind(cards, bid.parse().expect("invalid bid"))
+        })
         .collect();
 
     hands.sort_unstable();
-    
     (1..)
         .zip(hands)
         .map(|(rank, hand)| rank * hand.bid() as u32)
